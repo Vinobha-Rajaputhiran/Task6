d5f3e46925227886bd0a96a5bdcd4002e285d400
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-02-23 15:31:03 +0530

    Add files via upload

---
+++ Q4 Sum of 1st and last int.py
@@ -0,0 +1,7 @@
+input= int(input("Enter the integer: "))
+
+str_input = str(input)
+
+sum = int(str_input[0]) + int(str_input[-1])
+
+print("The sum of the first and last digit of the integer are: ", sum)
