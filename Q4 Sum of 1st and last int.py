9487e81082904f385ee86f90a353dbb575b07009
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:28:33 +0530

    Update Q4 Sum of 1st and last int.py

--- Q4 Sum of 1st and last int.py
+++ Q4 Sum of 1st and last int.py
@@ -1,7 +1,12 @@
+#Write a python program to find the sum of the first and last digit of an integer.
+
 input= int(input("Enter the integer: "))
+#Receive input from usesr
 
 str_input = str(input)
+#Convert the integer to string to fix index numbers.
 
 sum = int(str_input[0]) + int(str_input[-1])
+#Add the integer values of the first and last indexes of the string to get the solution.
 
 print("The sum of the first and last digit of the integer are: ", sum)
