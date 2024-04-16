b4d5bbd79b738ab4586c28030659fa5a8545c79d
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:36:26 +0530

    Update Q7 Non repeating values in a list.py

--- Q7 Non repeating values in a list.py
+++ Q7 Non repeating values in a list.py
@@ -1,9 +1,14 @@
+#Write a python program to find the non-repeating elements in a given list of integers.
+
 list= list(input("Enter the list of values: "). split())
+#Receive input list from user
 
 list2=[]
+#empty list
 
 for x in list:
     count=0
+    #counter set at zero. If the counter exceed zero, then the interger is a repeating.
     for y in list:
         if x==y:
             count=count+1
@@ -12,5 +17,6 @@ for x in list:
 
     if count==1:
         list2.append(x)
+        #Append the non-repeating elements to empty output list
 
 print("The list of non repeating values: ",list2)
