e86bc2e69657086a1412137d931fe92fec70228d
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:42:16 +0530

    Update Q8 Minimum element in list.py

--- Q8 Minimum element in list.py
+++ Q8 Minimum element in list.py
@@ -1,5 +1,10 @@
+#Write a Python program to find the minimum element in a rated and sorted list.
+
 list= list(input("Enter the list of elements: ").split())
+#Receive list input from user.
 
 newlist= sorted(list)
+#Utilizing the sorted method to sort the user list in ascending order.
 
 print("The minimum element in the given list is: ", newlist[0])
+#The index value of 0 contains the minimum element in the list.
