05253999717c43d461cadc4d7d140adfa032dc79
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:23:21 +0530

    Update Q2 Prime Number List.py

--- Q2 Prime Number List.py
+++ Q2 Prime Number List.py
@@ -1,7 +1,12 @@
+#You have been given a python list [10, 501, 22, 37, 100, 999, 87, 351]. The task is to create a new list containing all the prime numbers.
+
+
 list= [10, 501, 22, 37, 100, 999, 87, 351]
 
 primelist= []
+#Emty list is created to append all the prime numbers.
 
+#FOR loop will return all the prime values.
 for x in list:
 
     if x==0 or x<=1:
@@ -12,5 +17,6 @@ for x in list:
                 break
     else:
         primelist.append(x)
+        #Prime numbers appended to empty list
 
 print("Prime Numbers List: ", primelist)
