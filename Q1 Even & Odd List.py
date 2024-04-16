79ad3ec81319369c9a972e52561cf158ad0010d2
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:20:30 +0530

    Update Q1 Even & Odd List.py

--- Q1 Even & Odd List.py
+++ Q1 Even & Odd List.py
@@ -1,3 +1,5 @@
+#You have been given a python list [10, 501, 22, 37, 100, 999, 87, 351]. Your task is to create two separate lists containing even and odd numbers.
+
 list= [10, 501, 22, 37, 100, 999, 87, 351]
 #utilizing list comprehension to break the lists into two
 
