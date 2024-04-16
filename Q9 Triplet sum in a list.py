809210a44ade46c45d07953a2af368d73209bdd0
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:46:15 +0530

    Update Q9 Triplet sum in a list.py

--- Q9 Triplet sum in a list.py
+++ Q9 Triplet sum in a list.py
@@ -1,7 +1,11 @@
+#You have been given a Python list [10,20, 30,9] and a value of 59. Write a python program to find the triplet in the list whose sum is equal to the given value.
+
 list= [10,20, 30,9]
+#The input list and value are initialized.
 value=59
 counter=0
 
+#Using FOR  loop to iterate through different possibilities to find the triplet values that sum upto 59.
 for x in list:
     if counter==1:
         break
