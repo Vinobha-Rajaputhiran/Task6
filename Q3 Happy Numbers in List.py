dd9aafaffcb94900b52884f9df53a576541babd9
Vinobha-Rajaputhiran <160389353+Vinobha-Rajaputhiran@users.noreply.github.com>
2024-04-16 17:26:32 +0530

    Update Q3 Happy Numbers in List.py

--- Q3 Happy Numbers in List.py
+++ Q3 Happy Numbers in List.py
@@ -1,6 +1,12 @@
+#You have been given a python list [10, 501, 22, 37, 100, 999, 87, 351]. Create a new list with the ahappy numbers.
+
 input= [10, 501, 22, 37, 100, 999, 87, 351]
+#Input list
 
 HappyNumList=[]
+#Empty list to append values.
+
+#Function to filter out the happy numbers
 def CheckHappyNumber(n):
     digit = sum = 0
     while (n > 0):
@@ -8,7 +14,7 @@ def CheckHappyNumber(n):
         sum = sum + (digit * digit)
         n = n // 10
     return sum
-
+#FOR loop to loop the values of the list to filter the happy numbers.
 for x in input:
   result = x
 
